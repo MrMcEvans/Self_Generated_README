11165f89045403c1208de03d445e43f85ca92b12
MrMcEvans <125924206+MrMcEvans@users.noreply.github.com>
2023-07-14 13:48:26 -0500

    added functionality to table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -31,7 +31,7 @@ return '';}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-${renderLicenseBadge(data.license)}
+<img>${renderLicenseBadge(data.license)}<img>
 
 
 
@@ -43,19 +43,19 @@ ${renderLicenseBadge(data.license)}
 <h1>Table of Contents</h1>
 
 <h2>${renderLicenseLink(data.license)}</h2>
-<h2>Installation</h2>
-<h2>Usage</h2>
-<h2>Tests</h2>
-<h2>Contact me</h2>
+<h2> <a href=#install>Installation</a> </h2>
+<h2> <a href=#usage>Usage</a> </h2>
+<h2> <a href=#test>Tests</a> </h2>
+<h2> <a href=#question>Contact me</a> </h2>
 
 <h1>Installation</h1>
 
-<h2>${data.install}</h2>
+<h2 id="install">${data.install}</h2>
 
 
 <h1>Usage</h1>
 
-<h2>${data.usage}</h2>
+<h2 id="usage">${data.usage}</h2>
 
 
 <h1>Contributing</h1>
@@ -65,12 +65,12 @@ ${renderLicenseBadge(data.license)}
 
 <h1>Test</h1>
 
-<h2>${data.commandTest}</h2>
+<h2 id="test">${data.commandTest}</h2>
 
 <h2>${renderLicenseSection(data.license)}</h2>
 
 
-<h1>Questions</h1>
+<h1 id="question">Questions</h1>
 
 If you have any questions you can contact me at my GitHub (1) or by email (2)
 
